a15d1bca4006f92e5e9217fc350ca154c41a3a1d
Vanessa_Arce <vanessa.arce12@yahoo.com>
2019-07-30 15:06:13 -0400

    Added loop Scripts

---
+++ Mod 2/MyWork/LoopScripts/forLoop.js
@@ -0,0 +1,10 @@
+"use strict";
+let num=1;
+let i=1
+
+do 
+{
+    num="I love loops";
+    console.log(num);
+    i++;
+} while (i<=7)
---
+++ Mod 2/MyWork/LoopScripts/whileLoop.js
@@ -0,0 +1,10 @@
+"use strict";
+let num=1;
+let i=1;
+
+while (i<=7)
+{
+    num=" I love loops";
+    console.log(num);
+    i++;
+}
